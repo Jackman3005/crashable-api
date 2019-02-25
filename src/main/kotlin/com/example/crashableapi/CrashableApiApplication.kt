ada91c130534cd93e0064e1febe05b83fe55b0a2
Jack Coy <jackman3000@gmail.com>
2019-02-25 18:35:52 +1100

    Create a Crashable API

---
+++ src/main/kotlin/com/example/crashableapi/CrashableApiApplication.kt
@@ -0,0 +1,58 @@
+package com.example.crashableapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationContextAware
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.web.bind.annotation.*
+import java.time.Duration
+import java.time.LocalDateTime
+import kotlin.concurrent.thread
+
+@SpringBootApplication
+class CrashableApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<CrashableApiApplication>(*args)
+}
+
+@RestController
+class TestController {
+    @RequestMapping("/hello")
+    fun hello(): String {
+        return "Hello, the api is up and happily working!"
+    }
+
+    @RequestMapping("/doSomething/{seconds}")
+    fun hello(@PathVariable seconds : Long): String {
+        val millisToWait = seconds * 1000
+        val now = LocalDateTime.now()
+
+        while (true) {
+            if (Duration.between(now, LocalDateTime.now()).toMillis() > millisToWait){
+                break
+            }
+            println("Doing something for $seconds seconds!")
+            Thread.sleep(500)
+        }
+        return "Wow, that took awhile... $seconds seconds to be precise!"
+    }
+}
+
+@RestController
+class CrashController : ApplicationContextAware {
+    private lateinit var context: ConfigurableApplicationContext
+
+    @GetMapping("/crash")
+    fun crash(): String {
+        thread {
+            context.close()
+        }
+        return "OK"
+    }
+
+    override fun setApplicationContext(context: ApplicationContext) {
+        this.context = context as ConfigurableApplicationContext
+    }
+}
---
+++ src/test/kotlin/com/example/crashableapi/CrashableApiApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.example.crashableapi
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class CrashableApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
